xornet-sl/tigervnc

commit 4d06976d76c19e1ef673dec72ab31575e2ef865b

satisfy std c++98

common/rfb/H264Decoder.cxx
@@ -55,6 +55,8 @@ static LogWriter vlog("h264");
 
 H264DecoderContext::H264DecoderContext(const Rect& r) : rect(r)
 {
+  initialized = false;
+  h264AlignedCapacity = 0;
   os::AutoMutex lock(&mutex);
 
   if (!_initCodec())
@@ -559,12 +561,12 @@ bool H264Decoder::readRect(const Rect& r, rdr::InStream* is,
   os->writeU32(len);
   rdr::U32 flags = is->readU32();
 
-  if (flags & rectFlags::resetAllContexts)
+  if (flags & resetAllContexts)
   {
     resetContexts();
     if (!len)
       return false;
-    flags &= ~(rectFlags::resetContext | rectFlags::resetAllContexts);
+    flags &= ~(resetContext | resetAllContexts);
   }
   os->writeU32(flags);
 
@@ -606,7 +608,7 @@ void H264Decoder::decodeRect(const Rect& r, const void* buffer,
   rdr::U32 len = is.readU32();
   rdr::U32 flags = is.readU32();
 
-  if (flags & rectFlags::resetContext)
+  if (flags & resetContext)
     ctx->reset();
 
   if (!len)

common/rfb/H264Decoder.h
@@ -78,9 +78,9 @@ namespace rfb {
       SwsContext* sws = NULL;
       uint8_t* swsBuffer = NULL;
       rdr::U8* h264AlignedBuffer = NULL;
-      rdr::U32 h264AlignedCapacity = 0;
+      rdr::U32 h264AlignedCapacity;
 #endif
-      bool initialized = false;
+      bool initialized;
   };
 
   class H264Decoder : public Decoder {

commit 90e3567b9c00e3725b572245255f9e97c7eb67d7

4d06976 broke win32 build. fixed

common/rfb/H264Decoder.cxx
@@ -56,7 +56,9 @@ static LogWriter vlog("h264");
 H264DecoderContext::H264DecoderContext(const Rect& r) : rect(r)
 {
   initialized = false;
+#ifndef WIN32
   h264AlignedCapacity = 0;
+#endif
   os::AutoMutex lock(&mutex);
 
   if (!_initCodec())